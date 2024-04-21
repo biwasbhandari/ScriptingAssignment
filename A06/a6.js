8ce50d79cd44e407ee0dcf00f334d0e57f5b2f1e
xbiwas <biwas2059@gmail.com>
2024-04-21 17:36:26 +0545

    Assignment 6

---
+++ A06/a6.js
@@ -0,0 +1,19 @@
+// In JavaScript, events are actions or occurrences that happen in the web browser, such as clicking a button, pressing a key, or submitting a form. JavaScript allows you to respond to these events by defining event handlers, which are functions that execute when a specific event occurs.
+// Events in JavaScript are as follows:
+
+// Click Event:
+button.addEventListener("click", function () {});
+
+// KeyDown event:
+input.addEventListener("keydown", function (event) {});
+
+// Submit Event:
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+});
+
+// Load Event:
+image.addEventListener("load", function () {});
+
+// Resize Event:
+window.addEventListener("resize", function () {});

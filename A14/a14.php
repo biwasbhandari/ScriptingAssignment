7c557e400c95902020ba9728dd879dc3d4cc94d2
xbiwas <biwas2059@gmail.com>
2024-04-21 21:15:55 +0545

    Assignment 14

---
+++ A14/a14.php
@@ -0,0 +1,15 @@
+<?php
+
+// Function to add two numbers and return the result
+function add($num1, $num2) {
+    $sum = $num1 + $num2;
+    return $sum;
+}
+
+// Test the function with some values
+$num1 = 5;
+$num2 = 3;
+$result = add($num1, $num2);
+echo "The sum of $num1 and $num2 is: $result";
+
+?>
